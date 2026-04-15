65030c8b21ae4d160d507b2d53b9ae2f4713ae98
Leonn190 <euleonsoto@gmail.com>
2026-04-15 06:14:13 -0300

    ujh

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+estado = {"status": "run"}
+
+@app.route("/")
+def home():
+    return "API rodando"
+
+@app.route("/status", methods=["GET"])
+def get_status():
+    return jsonify(estado)
+
+@app.route("/set", methods=["POST"])
+def set_status():
+    data = request.json
+    if "status" in data:
+        estado["status"] = data["status"]
+    return jsonify(estado)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
